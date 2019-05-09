5ca3dbce885321cc65dd9cf08d40ce6f6f3be5ac
lanoflatfaceo <43626792+lanoflatfaceo@users.noreply.github.com>
2019-05-09 15:09:24 -0400

    more wqork

--- pandasaofnwe.py
+++ pandasaofnwe.py
@@ -1,19 +1,17 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
-s = pd.Series([1, 3, 5, np.nan, 6, 8])
-dates = pd.date_range('20130101', periods=6)
-df = pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
-df2 = pd.DataFrame({'A': 1.,
-                      'B': pd.Timestamp('20130102'),
-                       'C': pd.Series(1, index=list(range(4)), dtype='float32'),
-                       'D': np.array([3] * 4, dtype='int32'),
-                       'E': pd.Categorical(["test", "train", "test", "train"]),
-                        'F': 'foo'})
-
-df2 = df.copy()
-df2[df2 > 0] = -df2
-d=pd.read_csv('TANDS.csv')
+df=pd.read_csv('TANDS.csv')
 #df1 = df.reindex(index=dates[0:4], columns=list(df.columns))
-df.to_csv('foo.csv')
-print(d)
+ric=df["R"]
+deaths=df["D"]
+pop=df["pop"]
+deathsper=deaths/pop
+print(deathsper)
+
+
+
+
+plt.scatter(deathsper,ric)
+plt.show()
